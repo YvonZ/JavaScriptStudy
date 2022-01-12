f397e361d4d1c0daa287c33c2b9d26f04de0a6d8
YvonZ <97594645+YvonZ@users.noreply.github.com>
2022-01-12 11:35:40 +0200

    Update task1.js

--- HW1/task1.js
+++ HW1/task1.js
@@ -1,1 +1,11 @@
 
+//Перепишите код, заменив оператор `if` на тернарный оператор `?`
+
+/*js
+var result = ? ;
+if (a + b < 4) {
+result = true;
+} else {
+result = false;
+}*/
+var result = (a + b < 4) ? true : false;
