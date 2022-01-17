030f82fdba8066a7802c5260d0253b2332e1517e
YvonZ <97594645+YvonZ@users.noreply.github.com>
2022-01-18 01:34:04 +0200

    Update task10.js

--- HW1/task10.js
+++ HW1/task10.js
@@ -2,9 +2,9 @@ var arr = [6,5,4,3,2,1];
 for (let i = arr.length-1; i >= 0; i--) {
     for (let j = i; j >= 0; j--) {
         if (arr[i] < arr[j]) {
-          let temp = arr[i];
+          let tmp = arr[i];
           arr[i] = arr[j];
-          arr[j] = temp;
+          arr[j] = tmp;
         };
       };
     }
