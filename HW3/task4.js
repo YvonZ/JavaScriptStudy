749fa18720e85f239beaef3d5d96a874e6173774
YvonZ <97594645+YvonZ@users.noreply.github.com>
2022-01-30 00:52:06 +0200

    Create task4.js

---
+++ HW3/task4.js
@@ -0,0 +1,23 @@
+const arr = [1,2,3];
+
+function some(arr, callback) {
+    if (Array.isArray(arr) == false) {
+     console.log(arr," is not array");
+    } else if (typeof callback != 'function') {
+     console.log(callback," is not function");
+    } else {
+     //this;
+     for (let i=0; i<arr.length; i++){
+         if (!callback(arr[i], i, arr)){
+         } else {
+          console.log(true) 
+          return true}
+      }
+    }
+    console.log(false); 
+    return false
+    }
+
+some(arr, function(item, i, arr) {
+      return typeof item == "number"
+    });
